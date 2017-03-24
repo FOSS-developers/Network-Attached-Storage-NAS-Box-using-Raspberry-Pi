26b7db8ae1a34872c320cce49b817a63f46e1161
Nikhil Kathole <nikhilkathole2683@gmail.com>
2017-03-24 23:22:53 +0530

    Automatic Initial setup for app

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+os.system("sudo apt-get update")
+os.system("sudo apt-get upgrade -y")
+os.system("sudo apt-get install python-pip -y")
+os.system("sudo apt-get install python-opencv -y")
+os.system("sudo apt-get install mariadb-server -y")
+os.system("sudo apt-get install python-mysql.connector -y")
+os.system("sudo apt-get install apache2 -y")
+os.system("sudo apt-get install samba samba-common-bin -y")
+os.system("sudo apt-get install pure-ftpd -y")
+os.system("sudo pip install flask")
+os.system("sudo pip install flask-mail")
+os.system("echo -e \"\ny\ny\nroot\nroot\ny\nn\nn\ny\" | mysql_secure_installation")
+os.system("echo \"Enter MariaDb database password for root user(i.e. root)!!!\" ")
+os.system("sudo mysql -u root -p -e \"create database nas\" ")
+os.system("mysqldump -u root -p nas < nas.dump")
+os.system("echo \"Initial setup Successful! \" ")
